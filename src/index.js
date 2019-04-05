49a2030702a79a690846db9845775d598307c193
CesarRivasP <cesarjose58@gmail.com>
2019-04-05 14:11:12 -0400

    Implementation of graphQL in index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'https://mpjk0plp9.lp.gql.zone/graphql' }),
+  cache: new InMemoryCache()
+});
+
+const AppWithProvider = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
+
+ReactDOM.render(<AppWithProvider />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
