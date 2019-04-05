f585cd3bc084a422681f140875369af1949592fb
CesarRivasP <cesarjose58@gmail.com>
2019-04-05 14:52:22 -0400

    Implementation of example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const QUERY = gql`
+  {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
   }
-}
+`;
+
+const ExchangeRates = () => (
+  <Query query={QUERY}>
+    {
+      ({loading, error, data }) => {
+        if (error) return '💩 Oops!... ERRR';
+        if (loading) return 'Patience young grasshopper...';
+
+        return data.rates.map(({ currency, rate }) => (
+          <div key={currency}>
+            <p>{currency}: {rate}</p>
+          </div>
+        ))
+      }
+    }
+  </Query>
+);
+
+const App = () => <ExchangeRates />
 
 export default App;
